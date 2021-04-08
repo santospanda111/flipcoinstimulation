787568d63cdb5b287bd792625833b50fa650f341
santospanda111 <santospanda111@gmail.com>
2021-04-08 16:22:20 +0530

    [santospanda111] added flipcoinstimulation.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,23 +10,6 @@ fi
 head=1
 tail=0
 
-while [ $valid ]
-randomCheck=$(( RANDOM%2 ))
-do
-   if [ $randomCheck -eq $head ]
-   then
-      count=$(( $count+1 ))
-   else [ $randomCheck -eq $tail ]
-      count1=$(( $count1+1 ))
-   fi
-   if [[ $count -eq 11 || $count1 -eq 11 ]]
-   then
-      break
-   fi
-done
-
-echo "head wins "$count" times"
-echo "tail wins "$count1" times"
 
 while [ $valid ]
 randomCheck=$(( RANDOM%2 ))
@@ -56,4 +39,24 @@ then
    echo "tail wins head by "$num" times"
 else
 	echo "its a tie"
+fi
+
+if [ $count -eq $count1 ]
+then
+   while [ $valid ]
+   randomCheck=$(( RANDOM%2 ))
+   do
+      if [ $randomCheck -eq 1 ]
+      then
+         count=$(( $count+1 ))
+      else
+         count1=$(( $count1+1 ))
+      fi
+      num=$(( $count-$count1 ))
+      num1=$(( $count1-$count ))
+      if [[ $num -eq 2 || $num1 -eq 2 ]]
+      then
+         break
+      fi
+   done
 fi
